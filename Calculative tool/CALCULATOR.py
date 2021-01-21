6b54449fe3b550690eee3c89c5ba18152033386f
Ritvik-star <64014980+Ritvik-star@users.noreply.github.com>
2021-01-21 13:14:12 +0530

    calculator

---
+++ Calculative tool/CALCULATOR.py
@@ -0,0 +1,12 @@
+import _tkinter
+from tkinter import *
+import tkinter
+            
+
+root = tkinter.Tk()
+root.geometry("250x400+300+300")
+#root.resizable(0,0)
+root.title("CALCULATOR")
+
+
+root.mainloop()
